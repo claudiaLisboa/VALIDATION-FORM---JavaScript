d45d83672512a1ef1fc98f836d6ca133b4407b41
Claudia Lisboa <ccalumby@gmail.com>
2022-06-16 22:30:46 -0400

    updated formulaire

--- assets/scripts/Formulaire.js
+++ assets/scripts/Formulaire.js
@@ -7,25 +7,36 @@ class Formulaire {
      * Si valide, gestion du message 'Merci !'
      */
 
-     constructor(el) {
-        this._el = el;
-        // console.log(this._el);
-        this.init();
+     constructor(conteneur) {
+        this._conteneur = conteneur;
+
+        // Le vrai formulaire qui est dans le conteneur.
+        this._form = this._conteneur.firstElementChild;
+        // Pour empêcher le formulaire d'être soumis.
+        this._form.onsubmit = () => { return false; };
+
+        // Le bouton qui déclenchera la validation des champs.
+        this._button = this._conteneur.querySelector('[data-js-submit]');
+        this.addClickListener();
+
+        // Le message qui sera affiché si tout est valide.
+        this._message = this._conteneur.querySelector('[data-js-thank]');
     }
 
-    init() {
-        this.addEventListener();
-    }
-
-    addEventListener() {
-        const button = this._el.querySelector('[data-js-submit]');
-
-        button.addEventListener("click", () => {
+    addClickListener() {
+        this._button.addEventListener("click", () => {
             this.validate()
         }, false);
     }
 
     validate() {
-        alert('!');
+        const validator = new FormValidator(this._form);
+
+        if(validator.isValid()) {
+            // Si tout est valide, on supprime le formulaire...
+            this._form.classList.add('hidden');
+            // ... et on affiche le message final.
+            this._message.classList.remove('thank--hidden');
+        }
     }
 }
